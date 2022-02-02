dbb0b8bc0a3a047445c55c56f01e002ef8bb2ed0
lucasduran33 <lucasaduranlopez@gmail.com>
2022-02-02 11:48:14 -0300

    realizandocomponents

--- client/src/component/Home/Home.jsx
+++ client/src/component/Home/Home.jsx
@@ -2,7 +2,7 @@ import react from 'react'
 import {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux'
 import {getPokemon} from '../../redux/action/index'
-
+import NavBar from '../NavBar/NavBar'
 
 
 export default function Home () {
@@ -24,19 +24,8 @@ return (
     <div>
 
     <h1>La pokedex de duran</h1>
-    <Link to='/'>  <button>Crear pokemon</button>  </Link>
-    <button onClick={(e) => handleClick(e)}>Cargar todos los pokemones</button>
-    
-    <select>
-        <option value='asc'>A-Z</option>
-        <option value='des'>Z-A</option>
-    </select>
-    <select >
-                    <option value='puntuacion'>Puntuacion</option>
-                    <option value='mayorScore'>Mayor Fuerza</option>
-                    <option value='menorScore'>Menor Fuerza</option>
-    </select>
-    </div>
+   <NavBar/>
+   </div>
 )
 }
   
---
+++ client/src/component/NavBar/NavBar.jsx
@@ -0,0 +1,49 @@
+import react from 'react'
+import {useState, useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+
+export default function navBar () {
+    const dispatch = useDispatch()
+    const allPokemon = useSelector((state) => state.pokemons)
+    
+    
+    useEffect(() => { 
+    dispatch(getPokemon())
+    },[dispatch])
+    
+    
+    function handleClick (e){
+        e.preventDefault();
+        dispatch(getPokemon())
+    }
+    return (
+        <div>
+        <Link to='/'>  <button>Crear pokemon</button>  </Link>
+        <button onClick={(e) => handleClick(e)}>Cargar todos los pokemones</button>
+        
+        <select>
+            <option value='asc'>A-Z</option>
+            <option value='des'>Z-A</option>
+        </select>
+        <select >
+                        <option value='puntuacion'>Puntuacion</option>
+                        <option value='mayorScore'>Mayor Fuerza</option>
+                        <option value='menorScore'>Menor Fuerza</option>
+        </select>
+        <select>
+            <option>Tipo de pokemon</option>
+        </select>
+                        <option value='todos'>Todos</option>
+                        <option value='Api'>Existentes</option>
+                        <option value='baseDatos'>Creados</option>
+        <select>
+            option
+        </select>
+        </div>
+    )
+}
+    
+    
+    
